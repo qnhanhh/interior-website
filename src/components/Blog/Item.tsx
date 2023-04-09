21210cd635c78cf61e6a286c8fa69be1bcfc5dd7
qnhanhh <caoquynhanh18@gmail.com>
2023-04-09 16:39:52 +0700

    add blog section

--- src/components/Blog/Item.tsx
+++ src/components/Blog/Item.tsx
@@ -1,25 +1,38 @@
 import Image from "next/image";
 import { jostFont } from "@/utils/fonts";
-import image1 from "@/assets/blogs/blog-1.png";
+import images from "@/assets/blogs";
 import rightVector from "@/assets/rightVector.svg";
 
-export default function Item() {
+type itemType = {
+  info: {
+    imageIndex: number;
+    title: string;
+    date: string;
+    tag: string;
+  };
+};
+
+export default function Item({ info }: itemType) {
+  const { imageIndex, title, date, tag } = info;
+
   return (
-    <div className="p-8 rounded-[6.2rem] border-[1px] border-[#E7E7E7]">
-      <div>
-        <Image className="rounded-t-[6.2rem]" src={image1} alt="" />
-        <p className={`text-[1.6rem] capitalize ${jostFont.className}`}>
-          Kitchen design
+    <div className={`p-8 text-left rounded-[6.2rem] border-[1px] border-[#E7E7E7] ${imageIndex%2?'bg-[var(--primary-3)]':''}`}>
+      <div className="relative">
+        <Image
+          className="rounded-t-[6.2rem] w-full"
+          src={images[imageIndex]}
+          alt=""
+        />
+        <p
+          className={`absolute bottom-8 left-8 text-[1.6rem] rounded-[0.8rem] rounded-bl-none py-[0.6rem] px-[1rem] bg-white capitalize ${jostFont.className}`}
+        >
+          {tag}
         </p>
       </div>
-      <h3 className="capitalize">
-        Let&apos;s get solution for building construction work
-      </h3>
+      <h3 className="capitalize mt-8 mb-12">{title}</h3>
       <div className="flex justify-between items-center">
-        <p className={`text-[1.6rem] ${jostFont.className}`}>
-          26 December, 2022
-        </p>
-        <div className="w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[var(--primary-3)]">
+        <p className={`text-[1.6rem] ${jostFont.className}`}>{date}</p>
+        <div className={`w-[5rem] h-[5rem] flex items-center justify-center rounded-full ${imageIndex%2?'bg-white':'bg-[var(--primary-3)]'}`}>
           <Image src={rightVector} alt="" />
         </div>
       </div>
--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,17 +1,41 @@
 import { jostFont } from "@/utils/fonts";
-import images from "@/assets/blogs";
 import Item from "./Item";
+
+const blogData = [
+  {
+    imageIndex: 0,
+    title: "Let's get solution for building construction work",
+    date: "26 December, 2022",
+    tag: "Kitchen design",
+  },
+  {
+    imageIndex: 1,
+    title: "Low cost latest invented interior designing ideas",
+    date: "25 December, 2022",
+    tag: "Living design",
+  },
+  {
+    imageIndex: 2,
+    title: "Best for any office & business interior solution",
+    date: "22 December, 2022",
+    tag: "Interior design",
+  },
+];
 
 export default function Blog() {
   return (
     <div className="text-center">
       <h1>Articles & News</h1>
-      <p className={`large-para ${jostFont.className}`}>
+      <p className={`large-para mt-5 mb-20 ${jostFont.className}`}>
         It is a long established fact that a reader will be distracted by the of
         readable content of a page when lookings at its layouts the points of
         using.
       </p>
-      <Item />
+      <div className="grid grid-cols-3 gap-[4.8rem]">
+        {blogData.map((item, index) => {
+          return <Item key={index} info={item} />;
+        })}
+      </div>
     </div>
   );
 }
